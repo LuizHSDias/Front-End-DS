408ae0e20a4de41ba941a76ab3f915a863236d3f
LHSDias <luizhenriquemc15@gmail.com>
2025-07-07 01:00:58 -0300

    front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CadastrarDespesaComponent } from './pages/cadastrar-despesa/cadastrar-despesa.component';
 import { ConsultarDespesasComponent } from './pages/consultar-despesas/consultar-despesas.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent} ,
+    { path: 'home', component: HomeComponent} ,
     { path: 'cadastrar', component: CadastrarDespesaComponent },
     { path: 'cadastrar/:id', component: CadastrarDespesaComponent },
-    { path: 'consultar', component: ConsultarDespesasComponent }
+    { path: 'consultar', component: ConsultarDespesasComponent },
+    { path: '', component: LoginComponent}
 ];
